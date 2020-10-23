24f8b5cf5f430bcba26e75cdd351de9db8cfda4f
GasanNazer <40351273+GasanNazer@users.noreply.github.com>
2020-10-23 15:04:59 +0200

    including the library and Evaluator

---
+++ src/main/java/es/ucm/fdi/ici/PacmanEvaluator/Evaluate.java
@@ -0,0 +1,13 @@
+package es.ucm.fdi.ici.PacmanEvaluator;
+import es.ucm.fdi.ici.PacManEvaluator;
+import es.ucm.fdi.ici.Scores;
+
+public class Evaluate {
+
+	public static void main(String[] args) {
+		PacManEvaluator evaluator = new PacManEvaluator();
+		Scores scores = evaluator.evaluate();
+		scores.printScoreAndRanking();
+	}
+
+}
