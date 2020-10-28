61e81d3912fa73417be5cf4964174532783c75a6
GasanNazer <40351273+GasanNazer@users.noreply.github.com>
2020-10-28 22:27:58 +0100

    Cleaning comented not needed code. Ghosts P1 done!

--- src/main/java/es/ucm/fdi/ici/c2021/practica1/grupo06/Ghosts.java
+++ src/main/java/es/ucm/fdi/ici/c2021/practica1/grupo06/Ghosts.java
@@ -1,7 +1,6 @@
 package es.ucm.fdi.ici.c2021.practica1.grupo06;
 
 import java.util.EnumMap;
-import java.util.Random;
 
 import pacman.controllers.GhostController;
 import pacman.game.Constants.DM;
@@ -12,17 +11,13 @@ import pacman.game.Game;
 public class Ghosts extends GhostController {
 
 	private EnumMap<GHOST, MOVE> moves = new EnumMap<GHOST, MOVE>(GHOST.class);
-	private MOVE[] allMoves = MOVE.values();
-	private Random rnd = new Random();
 	private final static int LIMIT = 15;
 	private final static int GHOST_EATABLE_TIME_LIMIT = 100;
 	private final static int DISTANCE_TO_PACMAN = 100;
-	
 
 	@Override
 	public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue) {
 		moves.clear();
-		boolean ghostnear=false;
 		for (GHOST ghostType : GHOST.values()) {
 			if (game.doesGhostRequireAction(ghostType)) {
 
@@ -32,57 +27,31 @@ public class Ghosts extends GhostController {
 					int distanceToPacman = game.getShortestPathDistance(game.getGhostCurrentNodeIndex(ghostType),
 							game.getPacmanCurrentNodeIndex());
 
-					//System.out.println("Distance to Pacman: " + distanceToPacman);
-					//System.out.println(ghostType.name());
-					//System.out.println(ghostType.name() + " editable time: " + ghostTime);
-					//Parte aNASS
-					/*
-                     int distanceToPill=game.getShortestPathDistance(game.getGhostCurrentNodeIndex(ghostType),
-                    		 game.getClosestNodeIndexFromNodeIndex(game.getPacmanCurrentNodeIndex(),
-                      				game.getActivePowerPillsIndices(), DM.PATH));
-                     if(distanceToPill<distanceToPacman) {
-                    	 moves.put(ghostType,
- 								game.getApproximateNextMoveTowardsTarget(game.getGhostCurrentNodeIndex(ghostType),
- 										game.getClosestNodeIndexFromNodeIndex(game.getPacmanCurrentNodeIndex(),
- 			                      				game.getActivePowerPillsIndices(), DM.PATH), game.getGhostLastMoveMade(ghostType),
- 										DM.EUCLID));
-                     }
-                     */
-                     //FIN PARTE ANASS
-                     	if (ghostTime < GHOST_EATABLE_TIME_LIMIT && distanceToPacman < DISTANCE_TO_PACMAN) {
-                     		
-                     		/*if(ghostnear) {;//hacer acorralamiento 
-                     		}else {}
-                     		}*/
-						// System.out.println(ghostType.name());
-						// System.out.println(ghostType.name() + " editable time: " + ghostTime);
+					if (ghostTime < GHOST_EATABLE_TIME_LIMIT && distanceToPacman < DISTANCE_TO_PACMAN) {
 						moves.put(ghostType,
 								game.getApproximateNextMoveTowardsTarget(game.getGhostCurrentNodeIndex(ghostType),
 										game.getPacmanCurrentNodeIndex(), game.getGhostLastMoveMade(ghostType),
 										DM.EUCLID));
+
 						for (GHOST ghostType2 : GHOST.values()) {
 							if (!ghostType2.equals(ghostType)) {
-								moves.put(ghostType2, game.getApproximateNextMoveAwayFromTarget(
-										game.getGhostCurrentNodeIndex(ghostType2), game.getGhostCurrentNodeIndex(ghostType),
-										game.getGhostLastMoveMade(ghostType2), DM.EUCLID));
+								moves.put(ghostType2,
+										game.getApproximateNextMoveAwayFromTarget(
+												game.getGhostCurrentNodeIndex(ghostType2),
+												game.getGhostCurrentNodeIndex(ghostType),
+												game.getGhostLastMoveMade(ghostType2), DM.EUCLID));
 							}
 						}
-						ghostnear=true;
-
 					} else
 						moves.put(ghostType,
 								game.getApproximateNextMoveAwayFromTarget(game.getGhostCurrentNodeIndex(ghostType),
 										game.getPacmanCurrentNodeIndex(), game.getGhostLastMoveMade(ghostType),
 										DM.EUCLID));
+
 				} else {
-					//double randAux = rnd.nextFloat();
-					//if (randAux < 0.9)
-						moves.put(ghostType,
-								game.getApproximateNextMoveTowardsTarget(game.getGhostCurrentNodeIndex(ghostType),
-										game.getPacmanCurrentNodeIndex(), game.getGhostLastMoveMade(ghostType),
-										DM.EUCLID));
-						//else
-						//moves.put(ghostType, allMoves[rnd.nextInt(allMoves.length)]);
+					moves.put(ghostType,
+							game.getApproximateNextMoveTowardsTarget(game.getGhostCurrentNodeIndex(ghostType),
+									game.getPacmanCurrentNodeIndex(), game.getGhostLastMoveMade(ghostType), DM.EUCLID));
 				}
 
 			}
