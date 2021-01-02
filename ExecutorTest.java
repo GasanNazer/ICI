98b6667660b79bb602fc3aa78182065b8b36e659
GasanNazer <40351273+GasanNazer@users.noreply.github.com>
2021-01-02 11:22:56 +0200

    Practice 4 initial commit

--- ExecutorTest.java
+++ ExecutorTest.java
@@ -1,7 +1,8 @@
+
+
+import es.ucm.fdi.ici.practica4.demofuzzy.MsPacManFuzzy;
 import pacman.Executor;
 import pacman.controllers.GhostController;
-import pacman.controllers.HumanController;
-import pacman.controllers.KeyBoardInput;
 import pacman.controllers.PacmanController;
 
 
@@ -10,14 +11,14 @@ public class ExecutorTest {
     public static void main(String[] args) {
         Executor executor = new Executor.Builder()
                 .setTickLimit(4000)
-                .setGhostPO(false)
-                .setPacmanPO(false)
+                .setGhostPO(true)
+                .setPacmanPO(true)
                 .setVisual(true)
                 .setScaleFactor(2.0)
                 .build();
 
-        PacmanController pacMan = new HumanController(new KeyBoardInput());
-        GhostController ghosts = new es.ucm.fdi.ici.c2021.practica3.grupo06.Ghosts();
+        PacmanController pacMan = new MsPacManFuzzy();
+        GhostController ghosts = new GhostsRandom();
         
         System.out.println( 
         		executor.runGame(pacMan, ghosts, 40)
